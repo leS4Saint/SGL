365a494a401ca2777e2fae8cf29b235fae98b796
le S4-Saint <Huseyin_Unver@live.nl>
2016-11-17 23:02:56 +0100

    Add files via upload

---
+++ Menes/MeneSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeneSwitcher : MonoBehaviour
+{
+    //array of memes
+    public Material[] menes;
+
+    //renderer of the billboard
+    private Renderer bilbord;
+
+	void Start ()
+    {
+        //at start of game picks a random meme
+        bilbord = GetComponent<Renderer>();
+        bilbord.material = menes[Random.Range(0, menes.Length)];
+	}
+}
